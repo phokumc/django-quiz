829d00021310699fb607e2017b83c44676064266
Michael Phoku <phokumc@gmail.com>
2023-05-02 20:56:33 +0200

    Configure Django-Heroku

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from .views import quiz, home, score
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('quiz', quiz, name='quiz'),
     path('score/', score, name='score'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
